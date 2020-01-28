19fa983831926cbeea54e9a8143a9f57c676e111
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-01-28 19:33:45 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -42,8 +42,15 @@ def play(board)
     counter +=1
     if counter>=8
       break
-    elsif board == (board[0] == board[4] == board[8]) ||(board[2] == board[4] == board[6]) || (board[0] == board[1] == board[2]) || (board[3] == board[4] == board[5]) || (board[6] == board[7] == board[8]) || ((board[6] && board[7] && board[8]) == “O”)) || (((board[0] && board[3] && board[6]) == “X”) || ((board[0] && board[3] && board[6]) == “O”)) || (((board[1] && board[4] && board[7]) == “X”) || ((board[1] && board[4] && board[7]) == ”O”)) || (((board[2] && board[5] && board[8]) == “X”) || ((board[2] && board[5] && board[8]) == “O”))
-      break
+    elsif board == (board[0] == board[4] && board[4] == board[8])
+(board[2] == board[4] && board[4] == board[6])
+(board[0] == board[1] && board[1] == board[2])
+(board[3] == board[4] && board[4] == board[5])
+(board[6] == board[7] && board[7] == board[8])
+(board[0] == board[3] && board[3] == board[6])
+(board[1] == board[4] && board[4] == board[7])
+(board[2] == board[5] && board[5] == board[8])
+  break
     else
       turn(board)
     end
